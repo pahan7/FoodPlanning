d45a7e6fec01fd2188ecdaab173e707d37d5494d
Pavel <pahan7@inbox.lv>
2016-07-11 10:05:17 +0300

    Project start

---
+++ src/test/java/lv/ctco/jschool/FoodplanningApplicationTests.java
@@ -0,0 +1,18 @@
+package lv.ctco.jschool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FoodplanningApplication.class)
+@WebAppConfiguration
+public class FoodplanningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
