cd737a4738169197eded178d1de75526426f0247
Alina <vasilek93@inbox.lv>
2016-07-11 10:41:19 +0300

    classes

---
+++ src/main/java/Meal.java
@@ -0,0 +1,15 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Entity
+public class Meal {
+    @Id
+    @GeneratedValue
+    @Column
+    private int id;
+    @Column
+    private String mealName;
+
+
+}
---
+++ src/main/java/UserController.java
@@ -0,0 +1,16 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private List<User> users = new ArrayList<User>();
+}
