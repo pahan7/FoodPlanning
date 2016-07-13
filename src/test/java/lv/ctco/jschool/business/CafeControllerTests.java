1a4a2afaba2bebf608ee3a9b3d7d7a7d675c973c
N Okunev <nokunevs@gmail.com>
2016-07-13 09:43:45 +0300

    Bug with test fixed

--- src/test/java/lv/ctco/jschool/business/CafeControllerTests.java
+++ src/test/java/lv/ctco/jschool/business/CafeControllerTests.java
@@ -1,9 +1,18 @@
 package lv.ctco.jschool.business;
 
+import io.restassured.RestAssured;
 import io.restassured.http.Headers;
+import io.restassured.parsing.Parser;
+import lv.ctco.jschool.FoodplanningApplication;
 import lv.ctco.jschool.entities.Cafe;
 import lv.ctco.jschool.entities.User;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import static io.restassured.RestAssured.delete;
 import static io.restassured.RestAssured.get;
@@ -12,7 +21,18 @@ import static org.hamcrest.Matchers.equalTo;
 
 import static lv.ctco.jschool.Consts.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FoodplanningApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:8090")
+
 public class CafeControllerTests {
+
+    @Before
+    public void before() {
+        RestAssured.port = 8090;
+        RestAssured.defaultParser = Parser.JSON;
+    }
 
     @Test
     public void postCafeTest() {
